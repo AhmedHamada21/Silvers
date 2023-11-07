253a8ff19e4bbd5b90ce2a922f515673cd88b6e5
AhmedHamadaSliver <ahmedsilver2028@gmail.com>
2023-11-07 16:23:08 +0200

    edit

--- app/Console/Commands/CheckOrderHours.php
+++ app/Console/Commands/CheckOrderHours.php
@@ -75,7 +75,7 @@ class CheckOrderHours extends Command
 
                 $dataCheck = $ordersSaveHour->data . ' ' . $ordersSaveHour->hours_from;
 
-                if ($dataCheck < Carbon::now()->addMinutes(20)) {
+                if (Carbon::now()->addMinutes(20) < $dataCheck) {
                     sendNotificationUser($ordersSaveHour->user->fcm_token, 'لقد تم الغاء الرحله لعدم التأكيد', 'الغاء الرحله', true);
                     $ordersSaveHour->delete();
                 }
