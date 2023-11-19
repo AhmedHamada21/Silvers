a8eb8ed11dc3ed58a799fcdb4e61e00fb74a1842
AhmedHamadaSliver <ahmedsilver2028@gmail.com>
2023-11-19 16:53:41 +0200

    edit

--- app/Http/Controllers/Api/Users/AuthController.php
+++ app/Http/Controllers/Api/Users/AuthController.php
@@ -138,6 +138,7 @@ class AuthController extends Controller
     public function loginPhoneToken($user)
     {
         $information = User::findOrFail($user->id);
+        dd($information);
 
         if (!$information) {
             return $this->errorResponse('Unauthorized', 422);
