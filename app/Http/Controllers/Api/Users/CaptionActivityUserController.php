8aa67a40e90bc382b1500e36f8137d25e2000013
AhmedHamadaSliver <ahmedsilver2028@gmail.com>
2023-11-22 15:39:03 +0200

    edit

--- app/Http/Controllers/Api/Users/CaptionActivityUserController.php
+++ app/Http/Controllers/Api/Users/CaptionActivityUserController.php
@@ -201,7 +201,7 @@ class CaptionActivityUserController extends Controller
                 ->whereRaw("(6371 * acos(cos(radians($latitude)) * cos(radians(latitude)) * cos(radians(longitude) - radians($longitude)) + sin(radians($latitude)) * sin(radians(latitude)))) < $radius");
 
             if (!empty($carTypes)) {
-                $captainsNew = CarsCaption::whereIn('captain_id',$captains)->whereIn('car_type_id',$carTypes)->pluck('id');
+                $captainsNew = CarsCaption::whereIn('captain_id',$captains)->whereIn('car_type_id',$carTypes)->get();
                 dd($captainsNew);
             }
 
