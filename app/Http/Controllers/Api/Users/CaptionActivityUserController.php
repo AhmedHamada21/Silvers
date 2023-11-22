01d54611181a8789e93adba1c6ac718cb1638aef
AhmedHamadaSliver <ahmedsilver2028@gmail.com>
2023-11-22 16:15:39 +0200

    edit

--- app/Http/Controllers/Api/Users/CaptionActivityUserController.php
+++ app/Http/Controllers/Api/Users/CaptionActivityUserController.php
@@ -214,7 +214,7 @@ class CaptionActivityUserController extends Controller
 
             if (!empty($categoryCars) && !empty($carTypes)) {
                 $categoryCaptions = CarsCaption::whereIn('category_car_id', $categoryCars)->pluck('captain_id')->toArray();
-                $carTypeCaptains = CarsCaption::whereIn('car_type_id', $carTypes)->pluck('captain_id')->toArray();
+                $carTypeCaptains = CarsCaption::whereIn('car_type_id', [$carTypes])->pluck('captain_id')->toArray();
                 $combinedCaptains = array_merge($categoryCaptions, $carTypeCaptains);
                 $captains->whereIn('captain_id', $combinedCaptains);
             }
