ab7a79868c5f07fa90788b534b6c6405bdbfbdff
AhmedHamadaSliver <ahmedsilver2028@gmail.com>
2023-11-22 15:26:46 +0200

    edit

--- app/Http/Controllers/Api/Orders/OrderHourController.php
+++ app/Http/Controllers/Api/Orders/OrderHourController.php
@@ -163,7 +163,7 @@ class OrderHourController extends Controller
                 'order_code' => $orderCode,
                 'total_price' => $request->total_price,
                 'chat_id' => $chatId,
-                'status' => 'accepted',
+                'status' => 'pending',
                 'payments' => $request->payments,
                 'lat_user' => $request->lat_user,
                 'long_user' => $request->long_user,
