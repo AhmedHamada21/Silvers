32c6cc130bc747074ee052d73db345cdd0385712
AhmedHamadaSliver <ahmedsilver2028@gmail.com>
2023-11-22 15:45:05 +0200

    edit

--- app/Http/Controllers/Api/Users/CaptionActivityUserController.php
+++ app/Http/Controllers/Api/Users/CaptionActivityUserController.php
@@ -202,6 +202,7 @@ class CaptionActivityUserController extends Controller
 
             if (!empty($carTypes)) {
                 $carTypeCaptains = CarsCaption::whereIn('car_type_id', $carTypes)->pluck('captain_id')->toArray();
+                dd($carTypeCaptains);
                 $captains->whereIn('captain_id', $carTypeCaptains);
                 dd($captains);
             }
