b973bac2956f623ec42ccb07c9287e909ff96f3e
AhmedHamadaSliver <ahmedsilver2028@gmail.com>
2023-12-19 16:29:23 +0200

    edit

--- app/Services/Dashboard/CallCenter/CaptainService.php
+++ app/Services/Dashboard/CallCenter/CaptainService.php
@@ -10,18 +10,40 @@ class CaptainService
     public function getProfile($captainId)
     {
 
-        $findCaptions = CaptainProfile::where('uuid',$captainId)->first();
+        $findCaptions = CaptainProfile::where('uuid', $captainId)->first();
 
         $check = Captain::where('id', $findCaptions->captain_id)->first();
 
-        if ($check->callcenter_id) {
-            return redirect()->back()->with('error', 'Register the captain with another call center');
+        if ($check) {
+
+            $checkStatus = get_user_data()->type == "manager";
+            if ($checkStatus) {
+                return Captain::with(['profile'])->whereHas('profile', function ($query) use ($captainId) {
+                    $query->where('uuid', $captainId);
+                })->firstOrFail();
+            }
+
+
+            $checkUser = ($check->callcenter_id == true) == get_user_data()->id;
+
+
+            if ($checkUser) {
+                return Captain::with(['profile'])->whereHas('profile', function ($query) use ($captainId) {
+                    $query->where('uuid', $captainId);
+                })->firstOrFail();
+            } else {
+                return redirect()->route('CallCenterCaptains.index')->with('error', 'Register the captain with another call center');
+            }
+
+
         }
+
+
         $check->update([
             'callcenter_id' => auth('call-center')->id(),
         ]);
 
-       return Captain::with(['profile'])->whereHas('profile', function ($query) use ($captainId) {
+        return Captain::with(['profile'])->whereHas('profile', function ($query) use ($captainId) {
             $query->where('uuid', $captainId);
         })->firstOrFail();
     }
