fa81aa63781862714010ed787aef48a0a905d62b
ahmedhamada20 <hamada2031995@gmail.com>
2023-12-21 21:02:47 +0200

    edit

--- resources/views/layouts/common/includes/sidebar/call-center_sidebar.blade.php
+++ resources/views/layouts/common/includes/sidebar/call-center_sidebar.blade.php
@@ -54,8 +54,7 @@
                         </a>
                         <ul id="callcenters_managment" class="collapse" data-parent="#sidebarnav">
                             <li><a href="{{route('callCenters.index')}}">Call-Centers</a></li>
-                            <li><a href="{{route('callCenters.activity')}}">Call-Centers Activity</a></li>
-                            <li><a href="{{route('attendances.index')}}">Call-Centers Attendance</a></li>
+
                         </ul>
                     </li>
                     <!-- End Call Center Management Menu -->
